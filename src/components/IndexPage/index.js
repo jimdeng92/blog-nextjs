809a5facd04c4b3f0fa562178a70170c9c3f3016
jimdeng92 <jimdeng92@gmail.com>
2021-03-01 11:27:13 +0800

    feat(友盟): 添加友盟统计

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -17,6 +17,8 @@ const IndexPage = (props) => {
       <meta name="keywords" content={props.title || 'Jim\'s Space'} />
       <meta name="description" content={props.digest || 'JimDeng 的个人博客，记录生活点滴，学习心得。关于 CSS/JavaScript/Vue/React/Webpack/Node/Nextjs/小程序/面试'} />
       <link rel="icon" href="/favicon.ico" />
+      {/* 友盟统计 */}
+      <script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1279718538&web_id=1279718538"></script>
       <script src="//at.alicdn.com/t/font_2130365_bkgorptdsod.js"></script>
       <title>{props.title || 'Jim\'s Space'}</title>
     </Head>
