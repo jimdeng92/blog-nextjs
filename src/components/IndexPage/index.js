a71992561e3b29daee09777ba9bc94e6a08846bc
辰良 <chenliang@greatonce.com>
2022-11-11 17:51:15 +0800

    chore:添加脚本

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -18,7 +18,9 @@ const IndexPage = (props) => {
       <meta name="keywords" content={props.title || 'Jim\'s Space'} />
       <meta name="description" content={props.digest || 'JimDeng 的个人博客，记录生活点滴，学习心得。关于 CSS/JavaScript/Vue/React/Webpack/Node/Nextjs/小程序/面试'} />
       <link rel="icon" href="/j_logo.png" />
-      <link href="https://fonts.googlefonts.cn/css?family=Monda:400,700|PT+Mono|Roboto+Slab:300,400,700" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Monda:wght@400;700&family=PT+Mono&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       {/* 百度统计 */}
       <Script src="https://hm.baidu.com/hm.js?dfea8fd4a8de8465e7ac012f6e174cb1" />
       <title>{props.title || 'Jim\'s Space'}</title>
