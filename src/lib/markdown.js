a6313ec10c41a47e93684e3be53fae9f30318e6d
辰良 <chenliang@greatonce.com>
2022-11-30 14:52:25 +0800

    feat:收藏页修改

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -1,130 +1,80 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowLiteral": true}] */
 export default `
+<div style="height: 30px"></div>
+
+![](//imlinhe.com/static/images/2022/wallhaven-7p39gy_1280x720.png)
+
 ## 软件
 
 - [GHelper](https://ghelper.app/) 翻墙
 
-![image](https://user-images.githubusercontent.com/31594692/197317986-2ce8cc42-7373-4c34-8e30-e739ba2d0a7b.png)
-
 - [Listary](https://www.listary.com/) 本地检索
-
-![image](https://user-images.githubusercontent.com/31594692/197322344-ca5cc50c-78fc-4031-a5d1-8dc9b5534dd6.png)
 
 - [PowerShell](https://github.com/PowerShell/PowerShell) 命令行壳程序
 
-![image](https://user-images.githubusercontent.com/31594692/197322361-c7e133ae-3624-47ba-8985-33147a97771c.png)
-
-
 - [Tabby](https://github.com/Eugeny/tabby) 命令行壳程序
-
-![image](https://user-images.githubusercontent.com/31594692/197322375-d089fb9c-a54a-4b44-9222-945f81b57243.png)
-
 
 - [Bindizip](https://www.bandisoft.com/bandizip/) 文件压缩解压
 
-![image](https://user-images.githubusercontent.com/31594692/197322382-455df3c0-54c5-4f79-a585-8a14f3042897.png)
-
-
 - [MacType](https://github.com/snowie2000/MacType) windows 字体优化
-
-![image](https://user-images.githubusercontent.com/31594692/197322439-4361bb3b-e09b-4206-9d19-582f58deade1.png)
-
 
 - [Honeyview](https://www.bandisoft.com/honeyview/) 图片查看器
 
-![image](https://user-images.githubusercontent.com/31594692/197322459-ccd45690-d407-46b0-82b2-044b168d9435.png)
-
-
 - [PotPlayer](https://potplayer.daum.net) 视频播放器
-
-![image](https://user-images.githubusercontent.com/31594692/197322466-29f8cd92-383b-41d2-a5ac-3670296478df.png)
 
 - [XShell & XFTP](https://www.netsarang.com/zh/free-for-home-school) 服务器远程连接
 
-![image](https://user-images.githubusercontent.com/31594692/197322482-91b40c75-38b1-47a0-9220-b7c953fd25fe.png)
-
+- [snipaste](https://zh.Snipaste.com/) 截图工具
 
 ## 包
 
 - JavaScript
   - [JavaScript Standard Style](https://standardjs.com/) 代码规范
+
   - [whistle](https://github.com/avwo/whistle) 调试工具
+
   - [nvm-windows](https://github.com/coreybutler/nvm-windows) node 版本管理
+
   - [cross-env](https://github.com/kentcdodds/cross-env) 跨平台设置环境变量
+
   - [sequelize](https://github.com/sequelize/sequelize) 基于 Promise 的 Node ORM
+
   - [Puppeteer](https://github.com/puppeteer/puppeteer) Chromium 无头浏览器
+
   - [nodemon](https://nodemon.io/) 开发环境自动重启服务器
 
 - 其他
   - [Scrcpy](https://github.com/Genymobile/scrcpy) 安卓手机投屏
+
   - [Django](https://www.djangoproject.com/) Python 流行库
+
   - [Scrapy](https://scrapy.org/) Python 流行库
 
 ## 网页
 
 - [jex.im](https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24) 正则可视化
 
-![image](https://user-images.githubusercontent.com/31594692/197322493-a9082e58-102c-4b20-860b-9e12fd39b342.png)
-
-
 - [RegExr](https://regexr-cn.com/) 正则表达式测试
-
-![image](https://user-images.githubusercontent.com/31594692/197322511-e022dab1-3847-4a69-9d5e-f9498439ef0b.png)
-
 
 - [wallhaven](https://wallhaven.cc) 海量壁纸
 
-![image](https://user-images.githubusercontent.com/31594692/197322518-35674564-6a5f-47d3-8af6-8dce66454b94.png)
-
-
 - [TinyPNG](https://tinypng.com) 在线图片压缩
-
-![image](https://user-images.githubusercontent.com/31594692/197322522-3856743c-12cb-4a58-bdb1-59c5bb8db764.png)
-
 
 - [PicX](https://picx.xpoet.cn) 在线图床
 
-![image](https://user-images.githubusercontent.com/31594692/197322529-441cbf0f-c16c-4287-a856-426fe029b92d.png)
-
-
 - [carbon](https://carbon.now.sh) 代码美化
-
-![image](https://user-images.githubusercontent.com/31594692/197322537-45653ed6-71fd-45b7-882e-6531009f67d4.png)
-
 
 - [Rainyscope](https://rainyscope.com) 白噪音
 
-![image](https://user-images.githubusercontent.com/31594692/197322552-f9186da8-246f-4485-bbe3-1ece62334e5e.png)
-
-
 - [Hoppscotch](https://hoppscotch.io/) 接口测试
-
-![image](https://user-images.githubusercontent.com/31594692/197322545-68f119b1-6c8e-4561-a11d-7cc029fdb142.png)
-
 
 - [Lucide](https://lucide.dev/) 图标
 
-![image](https://user-images.githubusercontent.com/31594692/197322558-bb1a8c2c-83db-457f-be8e-5dcb9b13ffe5.png)
-
-
 - [ioDraw](https://www.iodraw.com/) 在线流程图
-
-![image](https://user-images.githubusercontent.com/31594692/197322569-c55548c5-5f50-48b0-ba0e-78a7d70f5e20.png)
-
 
 - [cdnjs](https://cdnjs.com/) CDN
 
-![image](https://user-images.githubusercontent.com/31594692/197322584-8ff706ad-cdeb-46c8-a6a0-3d5664e45e9d.png)
-
-
 - [manypixels](https://www.manypixels.co/) 插图
 
-![image](https://user-images.githubusercontent.com/31594692/197322637-d628f5a1-9642-4a95-b0a7-5641744a51d8.png)
-
-
 - [colorhunt](https://colorhunt.co/) 颜色
-
-![image](https://user-images.githubusercontent.com/31594692/197322655-03f061f1-3b61-49d5-8f30-ea4bacbe3efb.png)
-
-
 `
